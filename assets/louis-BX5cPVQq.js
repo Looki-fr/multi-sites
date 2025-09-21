b95c450aad78331ead156b1795e3f6b5ac6420fb
Looki-fr <lemeilllouis@gmail.com>
2025-09-21 14:09:56 +0200

    Updates

--- assets/louis-BX5cPVQq.js
+++
@@ -1,1 +0,0 @@
-import{a as t,j as e,R as o,L as r}from"./Louis-B0iXNTaZ.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
---
+++ assets/louis-DR66FMxE.js
@@ -0,0 +1,1 @@
+import{a as t,j as e,R as o,L as r}from"./Louis-Dem3Xha1.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
